c9eb55565750ebb08c8368f13c39f1435581523c
datamann1013 <141662792+datamann1013@users.noreply.github.com>
2025-06-06 17:54:21 +0200

    Update IUserRepository methods to return nullable types for better null handling

--- Chat-application-backend/Chat-application.API/Interfaces/IUserRepository.cs
+++ Chat-application-backend/Chat-application.API/Interfaces/IUserRepository.cs
@@ -4,8 +4,8 @@ namespace Chat_application.API.Interfaces;
 
 public interface IUserRepository
 {
-    Task<User> AddUserAsync(User user);
-    Task<User> GetUserByUsernameAsync(string username);
+    //Task<User> AddUserAsync(User user);
+    //Task<User> GetUserByUsernameAsync(string username);
     // Task<bool> DeleteUserAsync(Guid userId);
     Task<User?> AddUserAsync(User user);
     Task<User?> GetUserByUsernameAsync(string username);
